d5b5009aa5d9a590ceb481cd38d12d98edcb78a7
rsy <r.sy3244@gmail.com>
2020-11-29 01:57:00 +0900

    delete messages which are sent before 2h ago

--- chat.py
+++ chat.py
@@ -2,6 +2,8 @@ from discord.ext import commands
 import discord
 import os
 
+from datetime import datetime, timedelta
+
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
 GUILD_ID = int(os.environ['DISCORD_BOT_GUILD_ID'])
 CHANNEL_ID = int(os.environ['DISCORD_BOT_CHANNEL_ID'])
@@ -10,6 +12,7 @@ class ChatManager(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.chat_channel = None
+        self.lifetime = timedelta(hours=2)
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -19,6 +22,11 @@ class ChatManager(commands.Cog):
         print("""guild name: {}
 channel_name: {}""".format(guild, self.chat_channel))
 
+        #delete messages wich are sent before 2 hours ago
+        old_messages = await self.chat_channel.history(before=datetime.now()-self.lifetime).flatten()
+        await self.chat_channel.delete_messages(old_messages)
+
+
     @commands.Cog.listener()
     async def on_message(self, message):
         print("message {} in {}".format(message.content, message.channel))
@@ -27,7 +35,7 @@ channel_name: {}""".format(guild, self.chat_channel))
             print("delete message:")
             print(message)
             #遅延時間を設定できるようにする
-            await message.delete(delay=7200)
+            await message.delete(delay=self.lifetime.total_seconds())
             print("{} deleted".format(message.id))
 
 
