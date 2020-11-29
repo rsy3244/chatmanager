cf2ff66567f8e31e2f11caa0358c3f7ee6f7153b
rsy <r.sy3244@gmail.com>
2020-11-29 17:05:30 +0900

    add emoji expander

--- chat.py
+++ chat.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 import discord
 import asyncio
 import os
+import re
 from datetime import datetime, timedelta
 
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
@@ -13,33 +14,53 @@ class ChatManager(commands.Cog):
         self.bot = bot
         self.chat_channel = None
         self.lifetime = timedelta(hours=2)
+        self.emoji_pattern = re.compile(r'<a?:(\w+):(\d+)>')
+
+    def get_emoji(self, message):
+        print(message.content)
+        content = self.emoji_pattern.fullmatch(message.content)
+        if content != None:
+            print(content.groups())
+            return self.bot.get_emoji(int(content.group(2)))
+        else:
+            return None
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print("ready")
+        print(datetime.utcnow())
         guild = self.bot.get_guild(GUILD_ID)
         self.chat_channel = guild.get_channel(CHANNEL_ID)
         print("""guild name: {}
 channel_name: {}""".format(guild, self.chat_channel))
 
         #delete messages wich are sent before 2 hours ago
-        old_messages = await self.chat_channel.history(before=datetime.now()-self.lifetime).flatten()
+        print(datetime.utcnow()-self.lifetime)
+        old_messages = await self.chat_channel.history(before=datetime.utcnow()-self.lifetime).flatten()
+        for i in old_messages:
+            print(i)
         await self.chat_channel.delete_messages(old_messages)
-
+        late_messages = await self.chat_channel.history().flatten()
+        print(late_messages)
+        print("------------")
+        for message in late_messages:
+            life_time = (message.created_at + self.lifetime) - datetime.utcnow()
+            print("{}: {} sec".format(message.id, life_time.total_seconds()))
+            if life_time > timedelta(0):
+                await asyncio.sleep(life_time.total_seconds())
+            message.delete()
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        print("message {} in {}".format(message.content, message.channel))
-        print("{} {}".format(message.channel.id, self.chat_channel.id))
         if message.channel == self.chat_channel:
-            print("delete message:")
-            print(message)
             #遅延時間を設定できるようにする
-            print("timeleft: {}".format(self.lifetime.total_seconds()))
+            print("{} will be deleted".format(message.id))
             await asyncio.sleep(self.lifetime.total_seconds())
             await message.delete()
             print("{} deleted".format(message.id))
 
+            emoji = self.get_emoji(message)
+            if emoji != None:
+                await self.chat_channel.send(content=str(emoji.url))
 
 bot = commands.Bot(command_prefix='!')
 bot.add_cog(ChatManager(bot))
